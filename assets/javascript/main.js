cdfb1e0623a723b0699387f040b3bd4c4536e2d8
AbdiRabbani <abdirabbani59@gmail.com>
2022-11-17 07:58:34 +0700

    merge project from gharyn

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,13 +10,23 @@ setInterval(() => {
 
 const navHeader = document.querySelector('.navbar')
 const floating = document.querySelector('.floating-button')
+const icon = document.querySelector('.one')
+const icon2 = document.querySelector('.two')
+const icon3 = document.querySelector('.three')
+
 
 window.addEventListener('scroll', e => {
-    if(window.scrollY > 70) {
+    if (window.scrollY > 70) {
+        icon.style.left = '-50%';
+        icon2.style.right = '-50%';
+        icon3.style.right = '-50%';
         navHeader.classList.add('on-scroll')
         navHeader.classList.add('navbar-dark')
         floating.classList.add('floating-button-on-scroll')
-    }else {
+    } else {
+        icon.style.left = '20vw';
+        icon2.style.right = '20vw';
+        icon3.style.right = '35vw';
         navHeader.classList.remove('on-scroll')
         navHeader.classList.remove('navbar-dark')
         floating.classList.remove('floating-button-on-scroll')
